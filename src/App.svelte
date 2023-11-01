8fd0cefd818e5d173aeb8ef55181b80e2dfd8640
andresrestrepo <andresr.alz@gmail.com>
2023-11-01 14:30:26 -0500

    toats to show messages

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
-// @ts-nocheck
+  // @ts-nocheck
 
   import Number from "svelte-digital-alarm-number";
+  import { SvelteToast, toast } from "@zerodevx/svelte-toast";
 
   let level = 1;
   let is_running = false;
@@ -44,28 +45,81 @@
     clearInterval(inter);
 
     if (num === 10 && num2 === 0) {
-      console.log("gano");
+      toast.push("Good job!", {
+        theme: {
+          "--toastColor": "mintcream",
+          "--toastBackground": "rgba(72,187,120,0.9)",
+          "--toastBarBackground": "#2F855A",
+        },
+      });
+      if (level === 2) {
+        toast.push(`<strong>You win, go for a beer</strong>😛`, {
+          pausable: true,
+        });
+      }
     } else {
       retries = retries + 1;
-      console.log("intente de nuevo");
+      toast.push("Good try!", {
+        theme: {
+          "--toastColor": "mintcream",
+          "--toastBackground": "#f44336",
+          "--toastBarBackground": "#f44336",
+        },
+      });
     }
   }
 </script>
 
-<div>Try to stop the clock at 10:00</div>
+<h2>Try to stop the clock at 10:00</h2>
+
+<SvelteToast />
 
 <Number min="2" value={num} />
 <Number min="7" value={num2} />
 
 {#if is_running}
-  <button on:click={stop}>stop</button>
+  <button class="button button-stop" on:click={stop}>stop</button>
 {:else}
-  <button on:click={start}>start</button>
+  <button class="button button-start" on:click={start}>start</button>
 {/if}
 
-<select bind:value={level}>
-  <option value={1}>easy</option>
-  <option value={2}>hard</option>
+<br />
+Difficulty:
+<select
+  bind:value={level}
+  style="padding: 10px; background-color:rgb(217 118 16); border:none; color:black; font-size:1.5em; border-radius: 5px"
+>
+  <option value={1}>Noob</option>
+  <option value={2}>Pro</option>
 </select>
 
-<div>Attempts: {retries}</div>
+<h3>Attempts: {retries}</h3>
+
+<style>
+  .button {
+    border: none;
+    color: white;
+    padding: 30px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin-top: 15px;
+    margin-bottom: 15px;
+    font-size: 2em;
+    border-radius: 50%;
+    width: 150px;
+    height: 150px;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+      rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+      rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  }
+
+  .button-start {
+    background-color: #4caf50;
+  }
+
+  .button-stop {
+    background-color: #f44336;
+  }
+</style>
